04c376e9efb773825780beec60792ca2fce23c08
MoranSin <nurixhbh@gmail.com>
2025-05-24 21:11:41 +0300

    feat: final batchjob

--- email_notification.py
+++ email_notification.py
@@ -1,15 +1,40 @@
+import os
 import boto3
+from dotenv import load_dotenv
 from botocore.exceptions import ClientError
 
-def send_email_notification():
+load_dotenv()
+
+def send_email_notification(user_id, model_name, graph_type):
     ses_client = boto3.client('ses', region_name='eu-west-1')
-    subject = "NMA Graph Job Completed Successfully"
-    body = "The NMA explaination for the model <model_name> with <Graph-type> is ready!."
-    message = {"Subject": {"Data": subject}, "Body": {"Html": {"Data": body}}}
+    cognito_client = boto3.client('cognito-idp', region_name='eu-west-1')
+    try:
+        user = cognito_client.admin_get_user(
+            UserPoolId=os.environ.get("USER_POOL_ID"),
+            Username=user_id
+        )
+
+        email = next(attr['Value'] for attr in user['UserAttributes'] if attr['Name'] == 'email')
+        print("User email fetched successfully:", email)
+        
+    except ClientError as e:
+        print("Error fetching user email:", e.response['Error']['Message'])
+        return
+    
+    subject = "You're All Set - Your NMA Graph is ready!"
+    body_html = (
+        f"<h2>Hey, Deep Thinker!</h2>"
+        f"<p>Great news! The {graph_type} graph process for the model {model_name} is now complete, and you can officially log in to the system and start exploring.<br>We're excited to have you on board and expose you to BETTER model explanations.</p>"
+        f"<p>Welcome aboard!</p>"
+        f"<p>Best regards,<br>BETTER Team</p>"
+        f"<p><strong>Note:</strong> This is an automated message. Please do not reply to this email.</p>"
+    )
+    
+    message = {"Subject": {"Data": subject}, "Body": {"Html": {"Data": body_html}}}
     try:
         response = ses_client.send_email(
-            Source="<BETTER>@gmail.com",
-            Destination={"ToAddresses": ["<example>@gmail.com"]},
+            Source="betterxai2025@gmail.com",
+            Destination={"ToAddresses": [email]},
             Message=message
         )
         print("Email sent! Message ID:", response['MessageId'])
@@ -17,5 +42,7 @@ def send_email_notification():
         print("Error sending email:", e.response['Error']['Message'])
 
 if __name__ == "__main__":
-    send_email_notification()
-# This script sends an email notification using AWS SES when executed.
+    user_id = os.environ.get("user_id")
+    model_name = os.environ.get("model_name")
+    graph_type = os.environ.get("graph_type")
+    send_email_notification(user_id, model_name, graph_type)
